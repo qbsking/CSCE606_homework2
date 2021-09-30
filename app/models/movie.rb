550f02667edd0279f43208f86573ffff4a69ff0b
EC2 Default User <ec2-user@ip-172-31-9-3.us-east-2.compute.internal>
2021-09-30 21:58:30 +0000

    test

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,7 @@
 class Movie < ActiveRecord::Base
-    attr_accessible :title, :rating, :description, :release_date
     def self.all_ratings
-        %w(G PG PG-13 NC-17 R)
+        all_ratings = Movies.order(:rating).pluck(:rating).uniq # don't retrieve unnecessary datas
+        all_ratings << all_rating.delete('NC-17') # directly inject NC-17 at the end if exists
+        all_ratings.compact # remove nil values
     end
 end
