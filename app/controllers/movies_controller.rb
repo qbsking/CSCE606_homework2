8ef6ecf6f9311d9c0af46a1a941b3337ba9b0ef5
EC2 Default User <ec2-user@ip-172-31-9-3.us-east-2.compute.internal>
2021-09-30 20:17:35 +0000

    test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,9 +15,9 @@ class MoviesController < ApplicationController
     when 'release_date'
       #ordering,@date_header = {:order => :release_date}, 'hilite'
     end
-    @all_ratings = Movie.all_ratings
+    @selected_ratings = params[:ratings] || session[:ratings] || {}
     
-    @movies = Movie.all
+    @movies = Movie.find_all_by_rating(@selected_ratings.keys, ordering)
   end
 
   #def index
