8522dd9bd2a8eba125f5317b1b4f29551803b3ed
EC2 Default User <ec2-user@ip-172-31-9-3.us-east-2.compute.internal>
2021-09-30 19:34:32 +0000

    test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,6 +11,9 @@ class MoviesController < ApplicationController
     case sort
     when 'title'
       ordering,@title_header = {:order => :title}, 'hilite'
+    when 'release_date'
+      ordering,@date_header = {:order => :release_date}, 'hilite'
+    end
     @all_ratings = Movie.all_ratings
     @selected_ratings = params[:ratings] || session[:ratings] || {}
     
