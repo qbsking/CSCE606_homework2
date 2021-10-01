b3e74c28e7b609170ecfcb1f4d6392cffb4f98b6
EC2 Default User <ec2-user@ip-172-31-9-3.us-east-2.compute.internal>
2021-10-01 03:07:33 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,9 +13,11 @@ class MoviesController < ApplicationController
     
     @selected_ratings = params[:ratings] || session[:ratings] || {}
     
+    @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
+    
     if @selected_ratings == {}
       @movies = Movie.all
-      @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
+      
     end
     
     case sort
